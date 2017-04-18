e12b8fc85765272589796d0ad3d2a039a771e003
Prakash Gupta <guptap@codeaurora.org>
2017-04-18 07:32:20 +0530

    lowmemorykiller: fix scan_mutex contention
    A livelock can be created in the system by lowmemorykiller trying to kill
    the same task again and again.  Below is the livelock condition.
    
    P0 -> binder_main_lock(W)
    P1 -> binder_main_lock(T)-> mmap_sem(W)
    P2 -> mmap_sem(T) -> lowmem_scan::scan_mutex(W)
    P3 -> lowmem_scan::scan_mutex(T) -> send SIGKILL P0
    
    Here multiple tasks are contending on scan_mutex, and binder_main_lock.
    Change lowmem_scan mutex_lock with mutex_trylock, so in case of lowmem_scan
    lock contention, task will be allowed to reclaim from other shrinkers. This
    will also maintain the serialization of lowmemorykiller trigger.
    
    If a task is pending MEMDIE'ing, remove sleep before falling back on other
    shrinkers.
    
    If the task selected to be killed is MEMDIE'ing and in un-interruptible
    sleep state, do not repeat kill but fallback on other shrinkers without any
    delay.
    
    Change-Id: I12131622f7fa7b422c6d5d09f782af848300e412
    Signed-off-by: Prakash Gupta <guptap@codeaurora.org>
    (cherry picked from commit 1b8bb0da0b2dad2cc884580d1b38aab4cca78532)
    (cherry picked from commit e6ba007aeda01f0c2bf3e7d4cad12e5938794909)

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -217,6 +217,22 @@ static int test_task_flag(struct task_struct *p, int flag)
 	return 0;
 }
 
+static int test_task_state(struct task_struct *p, int state)
+{
+	struct task_struct *t;
+
+	for_each_thread(p, t) {
+		task_lock(t);
+		if (t->state & state) {
+			task_unlock(t);
+			return 1;
+		}
+		task_unlock(t);
+	}
+
+	return 0;
+}
+
 static DEFINE_MUTEX(scan_mutex);
 
 int can_use_cma_pages(gfp_t gfp_mask)
@@ -428,7 +444,7 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 	int other_free;
 	int other_file;
 
-	if (mutex_lock_interruptible(&scan_mutex) < 0)
+	if (!mutex_trylock(&scan_mutex))
 		return 0;
 
 	other_free = global_page_state(NR_FREE_PAGES);
@@ -540,6 +556,17 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 		long cache_size = other_file * (long)(PAGE_SIZE / 1024);
 		long cache_limit = minfree * (long)(PAGE_SIZE / 1024);
 		long free = other_free * (long)(PAGE_SIZE / 1024);
+
+
+		if (test_task_flag(selected, TIF_MEMDIE) &&
+		    (test_task_state(selected, TASK_UNINTERRUPTIBLE))) {
+			lowmem_print(2, "'%s' (%d) is already killed\n",
+				     selected->comm,
+				     selected->pid);
+			rcu_read_unlock();
+			mutex_unlock(&scan_mutex);
+			return 0;
+		}
 
 		task_lock(selected);
 		send_sig(SIGKILL, selected, 0);
